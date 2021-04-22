27630220ffbb8ab77647e23257c1dacbfa8a8d0d
Dev02 <Dev02@gmail.com>
2021-04-22 15:18:38 +0530

    dev2 modify test.java

--- NIT-Demo-Proj-02/src/main/java/in/akshayit/test/Test.java
+++ NIT-Demo-Proj-02/src/main/java/in/akshayit/test/Test.java
@@ -1,7 +1,10 @@
 package in.akshayit.test;
 
+import in.akshayit.beans.Utility;
+
 public class Test {
 public static void main(String[] args) {
-	
+	Utility u=new Utility();
+	System.out.println(u.getDate());
 }
 }
